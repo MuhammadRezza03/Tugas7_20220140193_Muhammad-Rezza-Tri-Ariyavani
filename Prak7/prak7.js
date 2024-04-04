f7907d56514415d2bc43980b97d90e0f3d482480
Muhammad Rezza <165622199+MuhammadRezza03@users.noreply.github.com>
2024-04-04 20:28:12 +0700

    Add files via upload

---
+++ Prak7/prak7.js
@@ -0,0 +1,13 @@
+video.addEventListener("click", function video(){
+    alert("Sang Dewa Nika telah kembali!!!");
+})
+
+audio.addEventListener("click", function audio(){
+    alert("Luffy Bernyanyi");
+})
+gambar.addEventListener("click", function gambar(){
+    alert("Trio Monster Straw Hat Pirates");
+})
+biodata.addEventListener("click", function biodata(){
+    alert("Daftar Bounty Bajak Laut Topi Jerami");
+})
